4572f0f6939b49e4c0564c657654d12230f7a58b
Kym Reilly <kymreilly@hotmail.com>
2022-07-15 20:29:01 +0930

    add friend, delete friend both operational

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -51,6 +51,32 @@ const userController = {
         res.status(500).json(err)
       })
   },
+  addFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.id },
+      { $addToSet: { friends: req.params.friendId } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err))
+  },
+  deleteFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.id },
+      { $pull: { friends: req.params.friendId } },
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => res.status(500).json(err))
+  },
 }
 
 module.exports = userController
